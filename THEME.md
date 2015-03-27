22130a423459dd4a52c493a9bbbc4a8c75fe9042
Jason van Zyl <jason@tesla.io>
2015-03-27 16:59:56 -0400

    IDE build with dark theme based on Chrometheme and Eclipse Color Theme

---
+++ THEME.md
@@ -0,0 +1,7 @@
+- A unified way to pick a theme
+  - disable the ECT UI, we just want the rendering capability
+  - Jeeeyul's UI is good for making the themes, but we don't want its presets
+  - we need to export all the CSS and editor preferences
+  - we need to be able the set colors and fonts, how do we integrate standard configurations here?
+  - can we embed fonts?
+- The desktop icon can't be on a transparent background
---
+++ product.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+
+# Copyright (c) 2013 to Jason van Zyl
+# All rights reserved. This program and the accompanying materials                                                                                                                                          
+# are made available under the terms of the Eclipse Public License v1.0                                                                                                                                     
+# which accompanies this distribution, and is available at                                                                                                                                                  
+# http://www.eclipse.org/legal/epl-v10.html   
+
+require 'fileutils'
+require 'mustache'
+require 'rexml/document'
+require 'yaml'
+include REXML
+
+product = YAML.load_file('product.yml')
+
+repositories = []
+features = []
+featureSets = []
+
+product['repos'].each do |repo|
+  repositories << repo
+end
+
+product['featureSets'].each do |featureSetHolder|
+  featureSetHolder.each do |id,featureSet|
+    unless featureSet['repo'].nil?
+      repository = { 'id' => id, 'url' => featureSet['repo'] }
+      repositories << repository
+    end
+    featureSets << featureSet
+    featureSet['features'].each do |feature|
+      features << feature
+    end
+  end
+end
+
+# We need to extract the repository and feature from each feature definition and
+# make it available in the product.
+product['repositories'] = repositories
+product['features'] = features
+
+# If the values are not copied into another Hash the mustache templates don't work?
+vars = {}
+product.each do |k,v|
+  vars[k] = v
+end
+
+def template(templateFile, target, vars)
+  template = File.open(templateFile).read
+  renderedText = Mustache.render(template, vars)  
+  File.open(target, 'w') { |f| 
+    f.write(renderedText) 
+  }
+end
+
+template('templates/pom.parent.mustache', 'pom.xml', vars)
+template('templates/plugin.mustache', product['pluginId'] + "/plugin.xml", vars)
+template('templates/pom.plugin.mustache', product['pluginId'] + "/pom.xml", vars)
+template('templates/feature.mustache', product['featureId'] + "/feature.xml", vars)
+template('templates/pom.feature.mustache', product['featureId'] + "/pom.xml", vars)
+
+productId = product['productId']
+
+template('templates/product.mustache', "#{productId}/#{productId}", vars)
+template('templates/pom.product.mustache', "#{productId}/pom.xml", vars)
+
+# Run Maven
+system("mvn clean package")
+
+# Copy the lombok.jar into place
+FileUtils.cp("lombok.jar", "io.tesla.ide.product/target/products/io.tesla.ide.product/macosx/cocoa/x86_64/TakariIDE.app/Contents/MacOS")
+
